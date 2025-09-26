3b906bfad3241e41dc8bcb639de2d04c9a6ee245
MrFritzBHS <fritzs@bedfordnhk12.net>
2025-09-25 21:01:21 -0400

    Create Student.java

---
+++ Unit-01/1_13-Object-Creation-and-Storage/Day-11-Academic-Helper/Student.java
@@ -0,0 +1,9 @@
+public class Student {
+  private String name;
+  private int finalPercentage;
+
+ public Student(String theName, int percent) {
+    name = theName;
+    finalPercentage = percent;
+  }
+}
